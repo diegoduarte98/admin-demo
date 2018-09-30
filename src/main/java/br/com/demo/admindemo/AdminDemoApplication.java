dba3a5e208b52f76ade407f474568e6eb31bbf4f
Diego Duarte <diegoduarte98@gmail.com>
2018-09-30 18:09:35 -0300

    primeiro commit

---
+++ src/main/java/br/com/demo/admindemo/AdminDemoApplication.java
@@ -0,0 +1,14 @@
+package br.com.demo.admindemo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDemoApplication.class, args);
+    }
+}
